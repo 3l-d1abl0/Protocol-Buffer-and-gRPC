91d0d96694d3b410d951db76885be1bcf2c36c1e
sameer <sameer.barha12@gmail.com>
2022-06-09 15:56:23 +0530

    added calculator service with sum api

--- client/client.js
+++ client/client.js
@@ -1,11 +1,12 @@
 let greets = require('../server/proto/greet_pb')
 let service = require('../server/proto/greet_grpc_pb')
 
+let calc = require('../server/proto/calculator_pb')
+let CalculatorService = require('../server/proto/calculator_grpc_pb')
+
 let grpc = require('grpc')
 
-function main(){
-
-    console.log('Hello from Client')
+function callGreetService(){
 
     let client = new service.GreetServiceClient(
         'localhost:50051',
@@ -30,7 +31,33 @@ function main(){
         }
     })
 
+}
 
+function callCalculatorService(){
+
+    let client = new CalculatorService.CalculatorServiceClient(
+        'localhost:50051',
+        grpc.credentials.createInsecure()
+    )
+
+    let sumRequest = new calc.SumRequest()
+    sumRequest.setNum1(100)
+    sumRequest.setNum2(500)
+
+    client.sum(sumRequest, (error, response) => {
+
+        if(!error){
+            console.log("Sum: ", response.getSumResult())
+        }else{
+            console.log('ERROR: ', error)
+        }
+    })
+}
+
+function main(){
+
+    //callGreetService()
+    callCalculatorService()
 
 }
 
--- server/index.js
+++ server/index.js
@@ -1,21 +1,35 @@
 var greets = require('../server/proto/greet_pb')
 var service = require('../server/proto/greet_grpc_pb')
 
+let calc = require('../server/proto/calculator_pb')
+let CalculatorService = require('../server/proto/calculator_grpc_pb')
 
 var grpc = require('grpc')
 
 /*
-    Implements the great RPC method.
+    Implements the greet RPC method.
 */
 function greet(call, callback){
 
     var greeting = new greets.GreetResponse()
 
     greeting.setResult(
-        "Hello "+call.request.getGreeting().getFirstName()
+        "Hello "+call.request.getGreeting().getLastName()+'\n'
+        +call.request.getGreeting().getFirstName()+' '+call.request.getGreeting().getLastName()
     )
 
     callback(null, greeting)
+}
+
+function sum(call, callback){
+
+    let sumResponse = new calc.SumResponse()
+
+    sumResponse.setSumResult(
+        call.request.getNum1()+call.request.getNum2()
+    )
+
+    callback(null, sumResponse)
 }
 
 
@@ -24,6 +38,8 @@ function main(){
     var Server = new grpc.Server()
 
     Server.addService(service.GreetServiceService, {greet: greet})
+
+    Server.addService(CalculatorService.CalculatorServiceService, {sum: sum})
 
     Server.bind("127.0.0.1:50051", grpc.ServerCredentials.createInsecure())
 
