1ab2292ebc8f2337400d24412d3a2a94c6a1bf55
sameer <sameer.barha12@gmail.com>
2022-06-09 13:03:55 +0530

    added unary rpc api

--- client/client.js
+++ client/client.js
@@ -1,20 +1,35 @@
-var grpc = require('grpc')
+let greets = require('../server/proto/greet_pb')
+let service = require('../server/proto/greet_grpc_pb')
 
-var services = require('../server/proto/dummy_grpc_pb')
-
-
+let grpc = require('grpc')
 
 function main(){
 
     console.log('Hello from Client')
 
-    var client = new services.DummyServiceClient(
+    let client = new service.GreetServiceClient(
         'localhost:50051',
         grpc.credentials.createInsecure()
     )
 
     //we do stuff
-    console.log("client: ",client);
+    let greeting = new greets.Greeting()
+    greeting.setFirstName("Jerry")
+    greeting.setLastName("Tom")
+
+
+    let request = new greets.GreetRequest()
+    request.setGreeting(greeting)
+
+    client.greet(request, (error, response) => {
+
+        if(!error){
+            console.log("Greeting Response: ", response.getResult())
+        }else{
+            console.log('ERROR: ', error)
+        }
+    })
+
 
 
 }
--- server/index.js
+++ server/index.js
@@ -1,10 +1,29 @@
+var greets = require('../server/proto/greet_pb')
+var service = require('../server/proto/greet_grpc_pb')
+
+
 var grpc = require('grpc')
+
+/*
+    Implements the great RPC method.
+*/
+function greet(call, callback){
+
+    var greeting = new greets.GreetResponse()
+
+    greeting.setResult(
+        "Hello "+call.request.getGreeting().getFirstName()
+    )
+
+    callback(null, greeting)
+}
 
 
 function main(){
 
     var Server = new grpc.Server()
 
+    Server.addService(service.GreetServiceService, {greet: greet})
 
     Server.bind("127.0.0.1:50051", grpc.ServerCredentials.createInsecure())
 
